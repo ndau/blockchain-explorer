6b63a420bd2d87d1c8fada0ab706a178afec675c
Derick Quartey <niikwartey1@gmsil.com>
2019-05-15 10:38:29 -0400

    remove obsolete code

--- src/components/organisms/navbar/index.js
+++ src/components/organisms/navbar/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import { Box, Image } from 'grommet'
-import Anchor from '../../atoms/anchor'
+import { Box } from 'grommet'
 import Container from '../../atoms/container'
 import Logo from '../../atoms/logo'
 import BlockchainSearch from '../../molecules/blockchainSearch'
 import { NAVBAR_COLOR } from '../../../constants'
-import logoFile from '../../../img/ndau_orange_logo.png'
 import './style.css'
 
 class Navbar extends Component {
@@ -16,15 +14,15 @@ class Navbar extends Component {
       <div className="Navbar">
         <Box fill background={this.background} elevation="medium">
           <Container topPad="0">
-              <Box margin={{vertical: "10px"}}>
-                <Box height="100%" direction="row" align="center" justify="between">
-                  <Logo />
-                  
-                  <Box margin={{left: "medium"}}>
-                    <BlockchainSearch browserHistory={this.props.browserHistory} />
-                  </Box>
+            <Box margin={{vertical: "10px"}}>
+              <Box height="100%" direction="row" align="center" justify="between">
+                <Logo />
+                
+                <Box margin={{left: "medium"}}>
+                  <BlockchainSearch browserHistory={this.props.browserHistory} />
                 </Box>
               </Box>
+            </Box>
           </Container>
         </Box>
       </div>
