bb37f7ae27a954e1c59fae9609a9927c5f4bfb7a
fet <fetmar@gmail.com>
2019-12-05 10:53:58 -0500

    merge leftovers

--- src/components/atoms/legend/index.js
+++ src/components/atoms/legend/index.js
@@ -1,42 +1,40 @@
 import React, { Component } from 'react'
-import { Box, Text } from "grommet"
+import { Box, Text } from 'grommet'
 import Keyword from '../../molecules/keyword'
 import './style.css'
 
 class Legend extends Component {
-  render() {
+  render () {
     const { label, value, inactive, keyword } = this.props
     return (
-      <Box 
+      <Box
         style={{
-          opacity: inactive ? "0.1" : "1", 
-          transition: "opacity 0.3s",
-          height: "25px"
+          opacity: inactive ? '0.1' : '1',
+          transition: 'opacity 0.3s',
+          height: '25px'
         }}
-        direction="row"
-      > 
-        <Box className="legendLabel" style={{textAlign: "right", marginRight: "3px", minWidth: "135px"}}>
-          <Text size="small" color="#ffe7c6">
-
-            { 
-<<<<<<< HEAD
-              keyword && 
-              <Text margin={{right: "xxsmall"}}><Keyword keyword={keyword} /></Text>
-=======
-              keyword &&
-              <Text margin={{right: "xxsmall"}} >
-                <Keyword keyword={keyword} size="small" />
+        direction='row'
+      >
+        <Box
+          className='legendLabel'
+          style={{ textAlign: 'right', marginRight: '3px', minWidth: '135px' }}
+        >
+          <Text size='small' color='#ffe7c6'>
+            {keyword && (
+              <Text margin={{ right: 'xxsmall' }}>
+                <Keyword keyword={keyword} size='small' />
               </Text>
->>>>>>> d3d810314f8a5e0f84864526aa2494b2794e6307
-            }
+            )}
             {label}:
           </Text>
         </Box>
-        <Box className="legendValue">
-          <Text size="small" weight="bold">{value}</Text>
+        <Box className='legendValue'>
+          <Text size='small' weight='bold'>
+            {value}
+          </Text>
         </Box>
       </Box>
-    );
+    )
   }
 }
 
--- src/components/organisms/transactionsList/index.js
+++ src/components/organisms/transactionsList/index.js
@@ -2,15 +2,20 @@ import React, { Component } from 'react'
 import { Box, Text } from 'grommet'
 import TransactionCard from '../../molecules/transactionCard'
 
-class TransactionsList extends Component{
+class TransactionsList extends Component {
   state = { activeTransactionIndex: null }
 
-  render() {
-    const { numberOfTransactions, transactionHashes, blockHeight, active } = this.props;
-    
-    if(parseInt(numberOfTransactions) === 0) {
+  render () {
+    const {
+      numberOfTransactions,
+      transactionHashes,
+      blockHeight,
+      active
+    } = this.props
+    console.log('TRANSACTION_HASHES', transactionHashes)
+    if (parseInt(numberOfTransactions) === 0) {
       return (
-        <Text size="medium" weight="bold">
+        <Text size='medium' weight='bold'>
           No transactions
         </Text>
       )
@@ -19,64 +24,69 @@ class TransactionsList extends Component{
     if (!transactionHashes) {
       return (
         <Box>
-          <Text onClick={this.toggleShowTransactions} as="span" color="#fff">
+          <Text onClick={this.toggleShowTransactions} as='span' color='#fff'>
             <b>transactions:</b>
           </Text>
 
-          <Box pad={{horizontal: "xlarge", vertical: "33px" }} animation="pulse">
-            <Text alignSelf="center" size="xsmall">Loading transactions...</Text>
+          <Box
+            pad={{ horizontal: 'xlarge', vertical: '33px' }}
+            animation='pulse'
+          >
+            <Text alignSelf='center' size='xsmall'>
+              Loading transactions...
+            </Text>
           </Box>
         </Box>
-      );
+      )
     }
 
-    
-    const gap = "small";
-    
+    const gap = 'small'
+
     return (
       <Box>
-        {
-          active &&
+        {active && (
           <Box>
-            <Text onClick={this.toggleShowTransactions} as="span" color="#fff">
+            <Text onClick={this.toggleShowTransactions} as='span' color='#fff'>
               <b>transaction{transactionHashes.length > 1 && 's'}:</b>
             </Text>
           </Box>
-        }
-        
+        )}
 
-        <Box 
+        <Box
           style={{
-            margin: "10px small 0px 15px",
+            margin: '10px small 0px 15px'
             // paddingLeft: "11px",
             // borderLeft: "1px solid rgba(255,255, 255, 0.3)"
           }}
         >
-          { 
-            transactionHashes.map((hash, index) => (
-              <Box key={index} margin={{bottom: (index !== transactionHashes.length-1)? gap : "0"}}>
-                <TransactionCard
-                  transactionHash={hash}
-                  blockHeight={blockHeight}
-                  open={index === this.state.activeTransactionIndex}
-                  index={index}
-                  setActiveTransaction={this.setActiveTransactionIndex}
-                />
-              </Box>
-            ))
-          }
+          {transactionHashes.map((hash, index) => (
+            <Box
+              key={index}
+              margin={{
+                bottom: index !== transactionHashes.length - 1 ? gap : '0'
+              }}
+            >
+              <TransactionCard
+                transactionHash={hash}
+                blockHeight={blockHeight}
+                open={index === this.state.activeTransactionIndex}
+                index={index}
+                setActiveTransaction={this.setActiveTransactionIndex}
+              />
+            </Box>
+          ))}
         </Box>
       </Box>
     )
   }
 
-  setActiveTransactionIndex = (index) => {
-    if(index !== this.state.activeTransactionIndex) {
+  setActiveTransactionIndex = index => {
+    if (index !== this.state.activeTransactionIndex) {
       this.setState({
         activeTransactionIndex: index
-      }) 
+      })
     }
   }
-} 
+}
 
-export default TransactionsList;
+export default TransactionsList
