3a102de66736be56ef94f65be161c37bbeb1d8b6
fet <fetmar@gmail.com>
2019-12-27 11:04:39 -0500

    make only expand/contract buttons clickable, change cursor on hover.

--- src/components/molecules/blockListItem/index.js
+++ src/components/molecules/blockListItem/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, Box } from "grommet"
+import { Text, Box } from 'grommet'
 import Anchor from '../../atoms/anchor'
 import { Expand, Contract } from 'grommet-icons'
 import Card from '../../atoms/card'
@@ -7,62 +7,70 @@ import Age from '../../atoms/age'
 import BlockDetails from '../../organisms/blockDetails'
 
 class BlockListItem extends Component {
-  render() {
-    const { block, active } = this.props;
+  render () {
+    const { block, active } = this.props
     if (!block) {
-      return <div></div>;
+      return <div></div>
     }
-  
-    const {height, timestamp} = block;
+
+    const { height, timestamp } = block
     return (
       <Card
-        header={(
+        header={
           <header>
             <Text>
-              <Text style={{float: 'right'}} >
-                { 
-                  active ? 
-                  <Contract size="12px" color="#777"/> 
-                  : 
-                  <Expand size="12px" color="#777"/>
-                }
+              <Text style={{ float: 'right' }}>
+                {active ? (
+                  <Contract
+                    style={{ cursor: 'pointer' }}
+                    onClick={this.toggleActiveState}
+                    size='12px'
+                    color='#777'
+                  />
+                ) : (
+                  <Expand
+                    style={{ cursor: 'pointer' }}
+                    onClick={this.toggleActiveState}
+                    size='12px'
+                    color='#777'
+                  />
+                )}
               </Text>
 
-              <Text size="medium">
-                <b>Block </b>  
+              <Text size='medium'>
+                <b>Block </b>
                 <Anchor
                   href={`/block/${height}`}
                   label={`#${height}`}
                   onClick={event => event.stopPropagation()}
                 />
               </Text>
-              
-              <Text size="xsmall" margin={{left: "medium"}} color="#aaa">
-                <i><Age timestamp={timestamp} suffix="ago"/></i>
+
+              <Text size='xsmall' margin={{ left: 'medium' }} color='#aaa'>
+                <i>
+                  <Age timestamp={timestamp} suffix='ago' />
+                </i>
               </Text>
             </Text>
           </header>
-        )}
-        onClick={this.toggleActiveState}
-        pad="15px"
+        }
+        pad='15px'
         // background={active ? "#0b1f3a" : "#0f2748"}
-        background={active ? "#0d2342" : "#0f2748"}
-        margin="xsmall"
+        background={active ? '#0d2342' : '#0f2748'}
+        margin='xsmall'
       >
-        <Box 
-          margin={{top: "10px"}}
-        >
+        <Box margin={{ top: '10px' }}>
           <BlockDetails block={block} active={active} />
         </Box>
       </Card>
-    );
+    )
   }
 
-  toggleActiveState = (event) => {
-    event.stopPropagation();
+  toggleActiveState = event => {
+    event.stopPropagation()
     const { active, setAsActiveBlock, unsetAsActiveBlock } = this.props
     return active ? unsetAsActiveBlock() : setAsActiveBlock()
   }
 }
 
-export default BlockListItem;
+export default BlockListItem
