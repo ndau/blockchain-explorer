71bddbecf204801df5fb63b68dff7ed075aed87d
fet <fetmar@gmail.com>
2019-12-27 12:12:19 -0500

    Add a quick check for addressyness (len=48, {0,2}="nd") and make the field an Anchor

--- src/components/molecules/value/index.js
+++ src/components/molecules/value/index.js
@@ -1,74 +1,84 @@
 import React from 'react'
-import { Box, Text } from "grommet"
-import TruncatedText from "../../atoms/truncatedText"
-import ExpandedTime from "../../atoms/expandedTime"
+import { Box, Text } from 'grommet'
+import TruncatedText from '../../atoms/truncatedText'
+import ExpandedTime from '../../atoms/expandedTime'
 import Keyword from '../../molecules/keyword'
 import { humanizeNumber } from '../../../helpers/format'
+import Anchor from '../../atoms/anchor'
 
-const Value = ({value, rawValue}) => {
+const isAddress = (addr = '') => {
+  if (addr.length === 48 && addr.substring(0, 2) == 'nd') {
+    return true
+  }
+  return false
+}
+
+const Value = ({ value, rawValue }) => {
   if (value !== 0 && !value) {
-    return null;
+    return null
   }
   const type = typeof value
   switch (type) {
     case 'object':
-      return <Values value={value} isArray={Array.isArray(value)} />;
+      return <Values value={value} isArray={Array.isArray(value)} />
     case 'string':
-      if(rawValue) {
+      if (rawValue) {
         return <ExpandedTime value={value} rawValue={rawValue} />
       }
-
-      return <TruncatedText value={value} />;
+      if (isAddress(value)) {
+        return <Anchor href={`/account/${value}`}>{value}</Anchor>
+      }
+      return <TruncatedText value={value} />
     case 'number':
       return humanizeNumber(value)
     default:
-      return value;
+      return value
   }
 }
 
-const Values = ({value, isArray}) => {
+const Values = ({ value, isArray }) => {
   if (isArray) {
     return (
-      <Box  
+      <Box
         style={{
-          margin: "5px 0px 0px 15px",
-          paddingLeft: "11px",
-          borderLeft: "1px solid rgba(255,255, 255, 0.3)"
+          margin: '5px 0px 0px 15px',
+          paddingLeft: '11px',
+          borderLeft: '1px solid rgba(255,255, 255, 0.3)'
         }}
       >
-      {
-        value.map((item, index) => {
-          return item && (
-            <Text key={index}>
-              <Value value={item} />
-            </Text>
+        {value.map((item, index) => {
+          return (
+            item && (
+              <Text key={index}>
+                <Value value={item} />
+              </Text>
+            )
           )
-        })
-      }
+        })}
       </Box>
     )
   }
 
   return (
-    <Box  
+    <Box
       style={{
-        margin: "5px 0px 0px 15px",
-        paddingLeft: "11px",
-        borderLeft: "1px solid rgba(255,255, 255, 0.3)"
+        margin: '5px 0px 0px 15px',
+        paddingLeft: '11px',
+        borderLeft: '1px solid rgba(255,255, 255, 0.3)'
       }}
     >
-      {
-        Object.keys(value).map((item, index) => {
-          const itemValue = value[item];
-          return itemValue && (
+      {Object.keys(value).map((item, index) => {
+        const itemValue = value[item]
+        return (
+          itemValue && (
             <Text key={index}>
               <Keyword label={item} />: <Value value={itemValue} />
             </Text>
           )
-        })
-      }
+        )
+      })}
     </Box>
-  );
+  )
 }
 
 export default Value
