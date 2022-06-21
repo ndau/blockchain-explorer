307017b40898b99cf62df263dec413fa23b43f68
CherryByteDev <developer@thecherrybyte.com>
2022-06-21 11:42:18 +0500

    NW-13 When market_price, display in $ format

--- src/components/molecules/value/index.js
+++ src/components/molecules/value/index.js
@@ -8,52 +8,52 @@
  * - -- --- ---- -----
  */
 
-import React from 'react'
-import { Box, Text } from 'grommet'
-import TruncatedText from '../../atoms/truncatedText'
-import ExpandedTime from '../../atoms/expandedTime'
-import Keyword from '../../molecules/keyword'
-import { humanizeNumber } from '../../../helpers/format'
-import Anchor from '../../atoms/anchor'
+import React from "react";
+import { Box, Text } from "grommet";
+import TruncatedText from "../../atoms/truncatedText";
+import ExpandedTime from "../../atoms/expandedTime";
+import Keyword from "../../molecules/keyword";
+import { humanizeNumber } from "../../../helpers/format";
+import Anchor from "../../atoms/anchor";
 
-const isAddress = (addr = '') => {
-  if (addr.length === 48 && addr.substring(0, 2) === 'nd') {
-    return true
+const isAddress = (addr = "") => {
+  if (addr.length === 48 && addr.substring(0, 2) === "nd") {
+    return true;
   }
-  return false
-}
+  return false;
+};
 
 const Value = ({ value, rawValue }) => {
   if (value !== 0 && !value) {
-    return null
+    return null;
   }
-  const type = typeof value
+  const type = typeof value;
   switch (type) {
-    case 'object':
-      return <Values value={value} isArray={Array.isArray(value)} />
-    case 'string':
+    case "object":
+      return <Values value={value} isArray={Array.isArray(value)} />;
+    case "string":
       if (rawValue) {
-        return <ExpandedTime value={value} rawValue={rawValue} />
+        return <ExpandedTime value={value} rawValue={rawValue} />;
       }
       if (isAddress(value)) {
-        return <Anchor href={`/account/${value}`}>{value}</Anchor>
+        return <Anchor href={`/account/${value}`}>{value}</Anchor>;
       }
-      return <TruncatedText value={value} />
-    case 'number':
-      return humanizeNumber(value)
+      return <TruncatedText value={value} />;
+    case "number":
+      return humanizeNumber(value);
     default:
-      return value
+      return value;
   }
-}
+};
 
 const Values = ({ value, isArray }) => {
   if (isArray) {
     return (
       <Box
         style={{
-          margin: '5px 0px 0px 15px',
-          paddingLeft: '11px',
-          borderLeft: '1px solid rgba(255,255, 255, 0.3)'
+          margin: "5px 0px 0px 15px",
+          paddingLeft: "11px",
+          borderLeft: "1px solid rgba(255,255, 255, 0.3)",
         }}
       >
         {value.map((item, index) => {
@@ -63,32 +63,37 @@ const Values = ({ value, isArray }) => {
                 <Value value={item} />
               </Text>
             )
-          )
+          );
         })}
       </Box>
-    )
+    );
   }
 
   return (
     <Box
       style={{
-        margin: '5px 0px 0px 15px',
-        paddingLeft: '11px',
-        borderLeft: '1px solid rgba(255,255, 255, 0.3)'
+        margin: "5px 0px 0px 15px",
+        paddingLeft: "11px",
+        borderLeft: "1px solid rgba(255,255, 255, 0.3)",
       }}
     >
       {Object.keys(value).map((item, index) => {
-        const itemValue = value[item]
+        let itemValue = value[item];
+        if (item === "market_price") {
+          itemValue = `$${(itemValue / 10 ** 11).toFixed(2)}`;
+          console.log(itemValue, "itemValue");
+          console.log(item, "item");
+        }
         return (
           itemValue && (
             <Text key={index}>
               <Keyword label={item} />: <Value value={itemValue} />
             </Text>
           )
-        )
+        );
       })}
     </Box>
-  )
-}
+  );
+};
 
-export default Value
+export default Value;
