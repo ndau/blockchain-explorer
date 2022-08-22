c739f442e0842590ef77158e1fc4487e87323a8e
awais-tech <71636588+awais-tech@users.noreply.github.com>
2022-08-22 15:47:25 +0500

    feat: add bookmark functionality

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -33,6 +33,7 @@ import api from "./api";
 import VerifyUser from "./components/pages/Registration/VerifyUser/VerifyUser";
 import ndaunodes from "./components/pages/ndaunodes/ndaunodes";
 import userBookmarks from "./components/pages/userBookmarks/userBookmarks";
+import About from "./components/pages/about/about";
 
 const Routes = () => {
   const loggedInContext = useContext(UserContext);
@@ -90,6 +91,7 @@ const Routes = () => {
         <Route path="/change-password/:token" component={ChangePassword} />
         <Route exact path="/verify-user/:token" component={VerifyUser} />
         <Route exact path="/profile" component={Profile} />
+        <Route exact path="/about" component={About} />
         <Route exact path="/richlist" component={Richlist} />
       </Switch>
     </BrowserRouter>
---
+++ src/components/pages/about/about.jsx
@@ -0,0 +1,10 @@
+import { Heading } from 'grommet';
+import React from 'react'
+import Page from '../../templates/page';
+const About = () => {
+    return (
+        <Page> <Heading style={{textAlign:"center"}}>About Us</Heading></Page>
+      );
+}
+ 
+export default About;
